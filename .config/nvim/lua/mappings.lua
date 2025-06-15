a0b2f9fddb61c6117880171c3bde551292b37e3b
Hamed Tajik <203373638+hmdtjk@users.noreply.github.com>
2025-06-15 08:56:18 +0330

    add keymaps to switch nvim buffers

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,3 +5,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>n", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>p", vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>b", vim.cmd.buffers)
