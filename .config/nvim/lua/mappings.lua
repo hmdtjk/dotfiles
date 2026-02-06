8bc2290b2a396c8f206afe432799dc5bffc5f9b7
Hamed Tajik <203373638+hmdtjk@users.noreply.github.com>
2026-02-06 18:24:17 +0330

    add netrw keymap to open file with xdg-open

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,3 +9,11 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>n", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>p", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>b", vim.cmd.buffers)
+
+vim.keymap.set("n", "<leader>o", function()
+    local file = vim.fs.joinpath(
+        vim.b.netrw_curdir,
+        vim.fn.eval("netrw#Call('NetrwGetWord')")
+    )
+    vim.fn.system({"xdg-open", file})
+end)
